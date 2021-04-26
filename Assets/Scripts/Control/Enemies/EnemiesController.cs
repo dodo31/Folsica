0a8bc6385ca0a7ba639176642cb6bf33fd42c41b
Dorian Roques <dorian-roques@orange.fr>
2021-04-26 12:56:58 +0200

    Imported and enemies Animated enemies

--- Assets/Scripts/Control/Enemies/EnemiesController.cs
+++ Assets/Scripts/Control/Enemies/EnemiesController.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 public class EnemiesController : MonoBehaviour
 {
+	private const string ENEMIES_PATH = "Models/Enemies/";
+
 	public Transform PlayArea;
 
 	public PathController[] Paths;
@@ -12,21 +14,66 @@ public class EnemiesController : MonoBehaviour
 	protected void Start()
 	{
 		enemies = new List<EnemyController>();
-		this.SpwanEnemy();
 	}
 
 	private void LateUpdate()
 	{
-        this.SpwanEnemy();
+		this.SpawnRandomEnemy();
 	}
 
-	public void SpwanEnemy()
+	public void SpawnRandomEnemy()
 	{
-		GameObject newEnemyObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
-		newEnemyObject.transform.localScale = new Vector3(0.1f, 0.05f, 0.05f);
-		newEnemyObject.transform.SetParent(PlayArea.transform);
+		float randomNumber = UnityEngine.Random.Range(0, 4);
 
-		EnemyController newEnemy = newEnemyObject.AddComponent<EnemyController>();
+		if (randomNumber < 1)
+		{
+			this.SpawRobotClassic();
+		}
+		else if (randomNumber >= 1 && randomNumber < 2)
+		{
+			this.SpawRobotHorse();
+		}
+		else if (randomNumber >= 2 && randomNumber < 3)
+		{
+			this.SpawRobotHealer();
+		}
+		else if (randomNumber >= 3 && randomNumber < 4)
+		{
+			this.SpawRobotCar();
+		}
+	}
+
+	public void SpawRobotClassic()
+	{
+		GameObject robotClassicObject = Resources.Load<GameObject>(ENEMIES_PATH + "Robot Classic");
+		this.SpwanEnemy(robotClassicObject);
+	}
+
+	public void SpawRobotHorse()
+	{
+		GameObject robotHorseObject = Resources.Load<GameObject>(ENEMIES_PATH + "Robot Horse");
+		this.SpwanEnemy(robotHorseObject);
+	}
+
+	public void SpawRobotHealer()
+	{
+		GameObject robotHealerObject = Resources.Load<GameObject>(ENEMIES_PATH + "Robot Healer");
+		this.SpwanEnemy(robotHealerObject);
+	}
+
+	public void SpawRobotCar()
+	{
+		GameObject robotCarObject = Resources.Load<GameObject>(ENEMIES_PATH + "Robot Car");
+		this.SpwanEnemy(robotCarObject);
+	}
+
+	public void SpwanEnemy(GameObject enemyPrefab)
+	{
+        GameObject enemyObject = Instantiate<GameObject>(enemyPrefab);
+		enemyObject.transform.localScale = Vector3.one;
+		enemyObject.transform.SetParent(PlayArea.transform);
+
+		EnemyController newEnemy = enemyObject.GetComponent<EnemyController>();
 		enemies.Add(newEnemy);
 
 		PathController path = this.SelectRandomPath();
