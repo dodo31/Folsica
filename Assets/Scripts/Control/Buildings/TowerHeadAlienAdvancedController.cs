41d22260b80215df694eb3ae806b06f30aa4dab6
Dorian Roques <dorian-roques@orange.fr>
2021-04-26 20:54:32 +0200

    Implemented placeholder classes for tower alien head

---
+++ Assets/Scripts/Control/Buildings/TowerHeadAlienAdvancedController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerHeadAlienAdvancedController : TowerHeadController
+{
+	public Vector3 TargetDirection(float range)
+	{
+		return Vector3.positiveInfinity;
+	}
+}
